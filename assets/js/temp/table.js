667bbeb01ac2712848584751cde4ae67723c9d60
Dimas Ardianto <dimasardnt6@gmail.com>
2023-06-09 20:30:13 +0700

    push

--- assets/js/temp/table.js
+++ assets/js/temp/table.js
@@ -4,26 +4,26 @@ export let isiTabel =
     <td>
     <div class="d-flex px-2 py-1">
         <div class="d-flex flex-column justify-content-center">
-        <h6 class="mb-0 text-sm">#KODEPOLI#</h6>
-        <h4 class="mb-0 text-sm">#NAMAPOLI#</h4>
+        <h7 class="mb-0 text-sm">#KODEPOLI#</h7>
+        <h8 class="mb-0 text-sm">#NAMAPOLI#</h8>
         </div>
     </div>
     </td>
-    <td>
-    <h6 class="mb-0 text-sm">#NAMAPASIEN#</h6>
-    <h4 class="mb-0 text-sm">#NOKTP#</h4>
+    <td align-middle text-center text-sm>
+    <h7 class="mb-0 text-sm">#NAMAPASIEN#</h7>
+    <h8 class="mb-0 text-sm">#NOKTP#</h8>
     </td>
     <td class="align-middle text-center text-sm">
-    <h6 class="mb-0 text-sm">#NOANTRIAN#</h6>
+    <h7 class="mb-0 text-sm">#NOANTRIAN#</h7>
     </td>
     <td class="align-middle text-center text-sm">
-    <h6 class="mb-0 text-sm">#TANGGALDAFTAR#</h6>
+    <h7 class="mb-0 text-sm">#TANGGALDAFTAR#</h7>
     </td>
     <td class="align-middle text-center text-sm">
-    <h6 class="mb-0 text-sm">#STATUS#</h6>
+    <h7 class="mb-0 text-sm">#STATUS#</h7>
     </td>
     <td class="align-middle">
-    <a href="#" class="btn btn-secondary btn-sm active" role="button" aria-pressed="true">Edit</a>
+    <a href="#" class="btn btn-outline-secondary btn-sm active" role="button" aria-pressed="true">Edit</a>
     <button type="button" class="btn btn-primary btn-sm">Delete</button>
     </td>
     </tr>

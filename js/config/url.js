3d548b5b51636713434cec075f3ba7a29643ab29
Dimas Ardianto <dimasardnt6@gmail.com>
2023-06-09 23:59:55 +0700

    push

--- js/config/url.js
+++ js/config/url.js
@@ -1,1 +1,2 @@
 export let urlAPIAntrian = "https://dimasardnt6-ulbi.herokuapp.com/all-antrian";
+export let urlAPIPasien = "https://dimasardnt6-ulbi.herokuapp.com/all-pasien";
--- js/controller/get.js
+++ js/controller/get.js
@@ -1,5 +1,5 @@
 import { addInner } from "https://bukulapak.github.io/element/process.js";
-import { isiTabel } from "../temp/table.js";
+import { isiTabel,isiTabel2 } from "../temp/table.js";
 export function isiTableAntrian(results) {
     results.forEach(isiRow);
 }
@@ -13,4 +13,18 @@ function isiRow(value) {
             .replace("#TANGGALDAFTAR#", value.tanggal_pendaftaran)
             .replace("#STATUS#", value.status_antrian)
         addInner("iniTabel", content);
+}
+
+export function isiTablePasien(results) {
+    results.forEach(isiRow2);
+}
+function isiRow2(value) {
+    let content =
+         isiTabel2.replace("#NAMAPASIEN#", value.nama_pasien)
+            .replace("#NOKTP#", value.nomor_ktp)
+            .replace("#ALAMAT#", value.nomor_ktp)
+            .replace("#NOHP#", value.nomor_telepon)
+            .replace("#LAHIR#", value.tanggal_lahir)
+            .replace("#GENDER#", value.jenis_kelamin)
+        addInner("iniTabel2", content);
 }

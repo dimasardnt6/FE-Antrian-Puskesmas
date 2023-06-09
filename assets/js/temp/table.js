1d5d1bb4e48cdac0a74a00f3b4398c3d39df6083
Dimas Ardianto <dimasardnt6@gmail.com>
2023-06-09 20:40:38 +0700

    push

--- assets/js/temp/table.js
+++ assets/js/temp/table.js
@@ -4,23 +4,23 @@ export let isiTabel =
     <td>
     <div class="d-flex px-2 py-1">
         <div class="d-flex flex-column justify-content-center">
-        <h6 class="mb-0 text-sm">#KODEPOLI#</h6>
-        <h6 class="mb-0 text-sm">#NAMAPOLI#</h6>
+        <h5 class="mb-0 text-sm">#KODEPOLI#</h5>
+        <h5 class="mb-0 text-sm">#NAMAPOLI#</h5>
         </div>
     </div>
     </td>
-    <td align-middle text-center text-sm>
-    <h6 class="mb-0 text-sm">#NAMAPASIEN#</h6>
-    <h6 class="mb-0 text-sm">#NOKTP#</h6>
+    <div class="d-flex flex-column justify-content-center">
+    <h5 class="mb-0 text-sm">#NAMAPASIEN#</h5>
+    <h5 class="mb-0 text-sm">#NOKTP#</h5>
+    </div>
+    <td class="align-middle text-center text-sm">
+    <h5 class="mb-0 text-sm">#NOANTRIAN#</h5>
     </td>
     <td class="align-middle text-center text-sm">
-    <h6 class="mb-0 text-sm">#NOANTRIAN#</h6>
+    <h5 class="mb-0 text-sm">#TANGGALDAFTAR#</h5>
     </td>
     <td class="align-middle text-center text-sm">
-    <h6 class="mb-0 text-sm">#TANGGALDAFTAR#</h6>
-    </td>
-    <td class="align-middle text-center text-sm">
-    <h6 class="mb-0 text-sm">#STATUS#</h6>
+    <h5 class="mb-0 text-sm">#STATUS#</h5>
     </td>
     <td class="align-middle">
     <a href="#" class="btn btn-outline-secondary btn-sm active" role="button" aria-pressed="true">Edit</a>

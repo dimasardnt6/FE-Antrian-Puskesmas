ac22361399c80145363b47a4823dd7575d777a0e
Dimas Ardianto <dimasardnt6@gmail.com>
2023-06-09 19:40:15 +0700

    push

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,6 +1,11 @@
-import { get } from "https://bukulapak.github.io/api/process.js"; 
+import { get } from "https://bukulapak.github.io/api/process.js";
+import { setInner } from "https://bukulapak.github.io/element/process.js";
 let urlAPI = "https://dimasardnt6-ulbi.herokuapp.com/all-antrian";
 get(urlAPI,isiTableAntrian);
 function isiTableAntrian(results){
     console.log(results);
+    results.forEach(isiRow);
+}
+function isiRow(value){
+    console.log(value)
 }

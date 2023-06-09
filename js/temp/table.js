e41e972cf887b309a1725a3d7553dfe316f95509
Dimas Ardianto <dimasardnt6@gmail.com>
2023-06-09 23:25:17 +0700

    push

--- js/temp/table.js
+++ js/temp/table.js
@@ -4,7 +4,7 @@ export let isiTabel =
     <td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
         <div class="flex px-2 py-1">
         <div>
-            <img src="../assets/img/poliklinik.png" class="inline-flex items-center justify-center mr-4 text-white transition-all duration-200 ease-soft-in-out text-sm h-9 w-9 rounded-xl" alt="user1" />
+            <img src="../assets/img/poli.png" class="inline-flex items-center justify-center mr-4 text-white transition-all duration-200 ease-soft-in-out text-sm h-9 w-9 rounded-xl" alt="user1" />
         </div>
             <div class="flex flex-col justify-center">
                 <h6 class="mb-0 leading-normal text-sm">#KODEPOLI#</h6>

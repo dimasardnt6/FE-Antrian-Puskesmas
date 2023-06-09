886e2590e4ed389488a6298a86ad160737f8a0e9
Dimas Ardianto <dimasardnt6@gmail.com>
2023-06-09 20:17:05 +0700

    push

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,11 +1,19 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
-import { setInner } from "https://bukulapak.github.io/element/process.js";
+import { addInner } from "https://bukulapak.github.io/element/process.js";
+import { isiTabel } from "./table.js";
 let urlAPI = "https://dimasardnt6-ulbi.herokuapp.com/all-antrian";
-get(urlAPI,isiTableAntrian);
-function isiTableAntrian(results){
-    console.log(results);
+get(urlAPI, isiTableAntrian);
+function isiTableAntrian(results) {
     results.forEach(isiRow);
 }
-function isiRow(value){
-    console.log(value)
+function isiRow(value) {
+    let content = 
+    isiTabel.replace("#KODEPOLI#", value.poli.kode_poliklinik)
+            .replace("#NAMAPOLI#", value.poli.nama_poliklinik)
+            .replace("#NAMAPASIEN#", value.identitas_pasien.nama_pasien)
+            .replace("#NOKTP#", value.identitas_pasien.nomor_ktp)
+            .replace("#NOANTRIAN#", value.nomor_antrian)
+            .replace("#TANGGALDAFTAR#", value.tanggal_pendaftaran)
+            .replace("#STATUS#", value.status_antrian)
+        addInner("iniTabel", content);
 }
---
+++ assets/js/temp/table.js
@@ -0,0 +1,31 @@
+export let isiTabel = 
+`
+    <tr>
+    <td>
+    <div class="d-flex px-2 py-1">
+        <div class="d-flex flex-column justify-content-center">
+        <h6 class="mb-0 text-sm">#KODEPOLI#</h6>
+        <h4 class="mb-0 text-sm">#NAMAPOLI#</h4>
+        </div>
+    </div>
+    </td>
+    <td>
+    <h6 class="mb-0 text-sm">#NAMAPASIEN#</h6>
+    <h4 class="mb-0 text-sm">#NOKTP#</h4>
+    </td>
+    <td class="align-middle text-center text-sm">
+    <h6 class="mb-0 text-sm">#NOANTRIAN#</h6>
+    </td>
+    <td class="align-middle text-center text-sm">
+    <h6 class="mb-0 text-sm">#TANGGALDAFTAR#</h6>
+    </td>
+    <td class="align-middle text-center text-sm">
+    <h6 class="mb-0 text-sm">#STATUS#</h6>
+    </td>
+    <td class="align-middle">
+    <a href="#" class="btn btn-secondary btn-sm active" role="button" aria-pressed="true">Edit</a>
+    <button type="button" class="btn btn-primary btn-sm">Delete</button>
+    </td>
+    </tr>
+    <tr>
+`

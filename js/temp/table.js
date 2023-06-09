f1f67caf76e4363d260470f8f64e6cee9d83ed00
Dimas Ardianto <dimasardnt6@gmail.com>
2023-06-09 22:52:34 +0700

    push

--- js/temp/table.js
+++ js/temp/table.js
@@ -7,29 +7,23 @@ export let isiTabel =
             <img src="../assets/img/user.png" class="inline-flex items-center justify-center mr-4 text-white transition-all duration-200 ease-soft-in-out text-sm h-9 w-9 rounded-xl" alt="user1" />
         </div>
             <div class="flex flex-col justify-center">
-                <h6 class="mb-0 leading-normal text-sm">#NAMA#</h6>
-                <p class="mb-0 leading-tight text-xs text-slate-400">#NPM#</p>
-                <p class="mb-0 leading-tight text-xs text-slate-400">#NOHP#</p>
+                <h6 class="mb-0 leading-normal text-sm">#KODEPOLI#</h6>
+                <p class="mb-0 leading-tight text-xs text-slate-400">#NAMAPOLI#</p>
             </div>
         </div>
     </td>
     <td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-        <p class="mb-0 font-semibold leading-tight text-xs">#NAMAPRODI#</p>
-        <p class="mb-0 font-semibold leading-tight text-xs">#JURUSAN#</p>
+        <p class="mb-0 font-semibold leading-tight text-xs">#NAMAPASIEN#</p>
+        <p class="mb-0 font-semibold leading-tight text-xs">#NOKTP#</p>
     </td>
     <td class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-        <p class="mb-0 font-semibold leading-tight text-xs">#KELAS#</p>
+        <p class="mb-0 font-semibold leading-tight text-xs">#NOANTRIAN#</p>
     </td>
     <td class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-        <p class="mb-0 font-semibold leading-tight text-xs">#PEMBAYARAN#</p>
+        <p class="mb-0 font-semibold leading-tight text-xs">#TANGGALDAFTAR#</p>
     </td>
     <td class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-        <p class="mb-0 font-semibold leading-tight text-xs">#MATKUL#</p>
-        <p class="mb-0 font-semibold leading-tight text-xs">#DOSEN#</p>
-    </td>
-    <td class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-        <p class="mb-0 font-semibold leading-tight text-xs">#ANGKA#</p>
-        <p class="mb-0 font-semibold leading-tight text-xs">#HURUF#</p>
+        <p class="mb-0 font-semibold leading-tight text-xs">#STATUS#</p>
     </td>
     <td class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
         <a class="inline-block px-2 py-1 mr-3 font-bold text-center text-white uppercase align-middle transition-all rounded-lg cursor-pointer bg-slate-500 leading-pro text-xs ease-soft-in tracking-tight-soft shadow-soft-md bg-150 bg-x-25 hover:scale-102 active:opacity-85 hover:shadow-soft-xs" href="edit-kemahasiswaan?kemahasiswaanId=#IDEDIT#">Edit</a>

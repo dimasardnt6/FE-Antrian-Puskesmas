feb04e5670ada3d12da8f9c1f1305494cf898d17
Marlina Lubis <lubismarlina5@gmail.com>
2023-06-10 20:33:45 +0700

    push

---
+++ js/config/url_put-dokter.js
@@ -0,0 +1,10 @@
+const urlParams = new URLSearchParams(window.location.search);
+const dokterId = urlParams.get('dokterId');
+
+export let urlPUT = "https://dimasardnt6-ulbi.herokuapp.com/upd-dokter/" + dokterId;
+
+export function AmbilResponse(result) {
+    console.log(result);
+    alert(result.message);
+    window.location.reload();
+}
---
+++ js/controller/post-dokter.js
@@ -0,0 +1,17 @@
+import { postData } from "https://bukulapak.github.io/api/process.js";
+import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
+import { urlPOST, AmbilResponse} from "../config/url_post-dokter.js";
+
+
+function pushData(){
+
+    let data = {
+        nama_dokter: getValue("nama_dokter"),
+        spesialis: getValue("spesialis"),
+        poliklinik: getValue("pliklinik")
+    }
+    postData(urlPOST, data, AmbilResponse);
+
+}
+
+onClick("button", pushData);

2316e96808f2ebde79a466f04a56904e8f990049
Dimas Ardianto <dimasardnt6@gmail.com>
2023-06-10 12:48:22 +0700

    push

---
+++ js/controller/delete.poli.js
@@ -0,0 +1,17 @@
+function deleteData(IDHAPUS) {
+    var poliklinikId = IDHAPUS;
+    var target_url = "https://dimasardnt6-ulbi.herokuapp.com/delete-poliklinik/" + poliklinikId;
+
+    var requestOptions = {
+        method: 'DELETE',
+        redirect: 'follow'
+    };
+
+    fetch(target_url, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            alert(result.message);
+            location.reload();
+        })
+        .catch(error => console.log('Error:', error));
+}

72653ffe4bccd8d8d3eb3771472060a4498a0f07
Dimas Ardianto <dimasardnt6@gmail.com>
2023-06-16 16:40:28 +0700

    push

--- js/config/url_post-poli.js
+++ js/config/url_post-poli.js
@@ -3,5 +3,5 @@ export let urlPOST = "https://dimasardnt6-ulbi.herokuapp.com/ins-poliklinik"
 export function AmbilResponse(result) {
     console.log(result); //menampilkan response API pada console
     alert(result.message); //menampilkan response API pada alert
-    window.location.href = "tabel-poli.html";
+    window.location.href = "tabel-poliklinik.html";
 }

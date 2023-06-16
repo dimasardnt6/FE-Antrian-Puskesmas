926ea7cc686ecc719006acbc5ce11b9f0f867a25
Dimas Ardianto <dimasardnt6@gmail.com>
2023-06-16 15:32:46 +0700

    push

--- js/controller/post-antrian.js
+++ js/controller/post-antrian.js
@@ -6,18 +6,16 @@ import { urlPOST, AmbilResponse} from "../config/url_post-antrian.js";
 function pushData(){
 
     let data = {
-
-        poli:{
+        poli: {
             kode_poliklinik: getValue("kode_poliklinik"),
             nama_poliklinik: getValue("nama_poliklinik"),
-        },
-        identitas_pasien:{
+            },
+            identitas_pasien: {
             nama_pasien: getValue("nama_pasien"),
-            nomor_ktp: getValue("nomor_ktp")
-        },
-        nomor_antrian: getValue("nomor_antrian"),
-        status_antrian: getValue("status_antrian"),
-                
+            nomor_ktp: getValue("nomor_ktp"),
+            },
+            nomor_antrian: parseInt(getValue("nomor_antrian")),
+            status_antrian: getValue("status_antrian"),
     }
     postData(urlPOST, data, AmbilResponse);
 

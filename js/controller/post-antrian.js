157ac8d615256c4117bfbe8deefe090e675c95ef
Marlina Lubis <lubismarlina5@gmail.com>
2023-06-16 15:16:25 +0700

    push

--- js/controller/post-antrian.js
+++ js/controller/post-antrian.js
@@ -7,12 +7,16 @@ function pushData(){
 
     let data = {
 
+        poli:{
             kode_poliklinik: getValue("kode_poliklinik"),
             nama_poliklinik: getValue("nama_poliklinik"),
+        },
+        identitas_pasien:{
             nama_pasien: getValue("nama_pasien"),
-            nomor_ktp: getValue("nomor_ktp"),
-            nomor_antrian: getValue("nomor_antrian"),
-            status_antrian: getValue("status_antrian"),
+            nomor_ktp: getValue("nomor_ktp")
+        },
+        nomor_antrian: getValue("nomor_antrian"),
+        status_antrian: getValue("status_antrian"),
                 
     }
     postData(urlPOST, data, AmbilResponse);

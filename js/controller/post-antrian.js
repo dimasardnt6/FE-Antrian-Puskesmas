428a612f970b92824c7d26ae0de6ae8ef2089b04
Marlina Lubis <lubismarlina5@gmail.com>
2023-06-16 15:02:41 +0700

    push

---
+++ js/controller/post-antrian.js
@@ -0,0 +1,27 @@
+import { postData } from "https://bukulapak.github.io/api/process.js";
+import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
+import { urlPOST, AmbilResponse} from "../config/url_post-antrian.js";
+
+
+function pushData(){
+
+    let data = {
+        poli: {
+            kode_poliklinik: getValue("kode_poliklinik"),
+            nama_poliklinik: getValue("nama_poliklinik")
+        },
+        pasien: {
+            nama_pasien: getValue("nama_pasien"),
+            nomor_ktp: getValue("nomor_ktp")
+        },
+        antrian: {
+            nomor_antrian: getValue("nomor_antrian"),
+            status_antrian: getValue("status_antrian")
+        }
+        
+    }
+    postData(urlPOST, data, AmbilResponse);
+
+}
+
+onClick("button", pushData);

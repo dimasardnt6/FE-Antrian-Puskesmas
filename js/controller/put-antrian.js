f61161c0b26bfad35b75029ca98c427f66bacd99
Dimas Ardianto <dimasardnt6@gmail.com>
2023-07-05 15:41:08 +0700

    push

--- js/controller/put-antrian.js
+++ js/controller/put-antrian.js
@@ -33,60 +33,59 @@ function pushData(){
     let statusantrianValue = getValue("status_antrian");
 
    // form validation
-   if (kodepoliklinikValue === "" && namapoliklinikValue === "" && namapasienValue === "" && nomorktpValue === "" && statusantrianValue === "") {
-    Swal.fire({
-    icon: 'error',
-    title: 'Data tidak boleh kosong!',
-    text: '',
-    showConfirmButton: false,
-    timer: 2000
+    if (kodepoliklinikValue === "" && namapoliklinikValue === "" && namapasienValue === "" && nomorktpValue === "" && statusantrianValue === "") {
+        Swal.fire({
+        icon: 'error',
+        title: 'Data tidak boleh kosong!',
+        text: '',
+        showConfirmButton: false,
+        timer: 2000
+        });
+        return;
+    }
+    
+    if (statusantrianValue === "") {
+        Swal.fire({
+        icon: 'error',
+        title: 'Status Antrian tidak boleh kosong!',
+        text: '',
+        showConfirmButton: false,
+        timer: 2000
+        });
+        cek = false;
+    }  
+
+    Promise.all([getpoliData(kodepoliklinikValue,namapoliklinikValue), getpasienData(namapasienValue,nomorktpValue,nomorteleponValue)])
+    .then(([poliData, pasienData]) => {
+      // Ekstrak nilai-nilai yang diperlukan dari data yang diambil
+        let kodepoliText = poliData.kode_poliklinik;
+        let namapoliValue = poliData.nama_poliklinik;
+
+        let namapasienText = pasienData.nama_pasien;
+        let noktpValue = pasienData.nomor_ktp;
+        let noteleponValue = pasienData.nomor_telepon;
+
+        // Bangun objek data
+        let data = {
+            poli:{
+                _id: kodepoliklinikValue,
+                kode_poliklinik: kodepoliText,
+                nama_poliklinik: namapoliValue,
+            },
+            identitas_pasien:{
+                _id: namapasienValue,
+                nama_pasien: namapasienText,
+                nomor_ktp: noktpValue,
+                nomor_telepon: noteleponValue,
+            },
+            status_antrian: statusantrianValue,
+        };
+        console.log(data);
+        putData(urlPUT, data, AmbilResponse);
+    })
+    .catch((error) => {
+    console.error(error);
     });
-    return;
-}
+    }
 
-if (statusantrianValue === "") {
-    Swal.fire({
-    icon: 'error',
-    title: 'Status Antrian tidak boleh kosong!',
-    text: '',
-    showConfirmButton: false,
-    timer: 2000
-    });
-    cek = false;
-}   
-
-Promise.all([getpoliData(kodepoliklinikValue,namapoliklinikValue), getpasienData(namapasienValue,nomorktpValue,nomorteleponValue)])
-.then(([poliData, pasienData]) => {
-  // Ekstrak nilai-nilai yang diperlukan dari data yang diambil
-    let kodepoliText = poliData.kode_poliklinik;
-    let namapoliValue = poliData.nama_poliklinik;
-
-    let namapasienText = pasienData.nama_pasien;
-    let noktpValue = pasienData.nomor_ktp;
-    let noteleponValue = pasienData.nomor_telepon;
-
-    // Bangun objek data
-    let data = {
-        poli:{
-            _id: kodepoliklinikValue,
-            kode_poliklinik: kodepoliText,
-            nama_poliklinik: namapoliValue,
-        },
-        identitas_pasien:{
-            _id: namapasienValue,
-            nama_pasien: namapasienText,
-            nomor_ktp: noktpValue,
-            nomor_telepon: noteleponValue,
-        },
-        nomor_antrian: nomorantrianValue,
-        status_antrian: statusantrianValue,
-    };
-    console.log(data);
-    putData(urlPUT, data, AmbilResponse);
-})
-.catch((error) => {
-console.error(error);
-});
-}
-
-onClick("button", pushData);
+onClick("buttonAntrian", pushData);

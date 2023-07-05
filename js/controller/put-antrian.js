b18a039e5f7c416b72ca9787fe2f387f96b9f3bb
Dimas Ardianto <dimasardnt6@gmail.com>
2023-07-05 16:50:25 +0700

    push

--- js/controller/put-antrian.js
+++ js/controller/put-antrian.js
@@ -24,16 +24,27 @@ async function getpoliData(poliklinikId) {
   }
   
 
-async function getpasienData(pasienId) {
-    // Fetch school data based on the ID (replace with your API endpoint)
-    const response = await fetch(`https://dimasardnt6-ulbi.herokuapp.com/pasien/${pasienId}`);
+  async function getpasienData(pasienId) {
+    const timeoutDuration = 5000; // Waktu timeout dalam milidetik (misalnya, 5000 ms = 5 detik)
+    
+    // Fetch patient data based on the ID (replace with your API endpoint)
+    const responsePromise = fetch(`https://dimasardnt6-ulbi.herokuapp.com/pasien/${pasienId}`);
+    
+    const timeoutPromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject(new Error('Request timed out.'));
+      }, timeoutDuration);
+    });
+    
+    const response = await Promise.race([responsePromise, timeoutPromise]);
+    
     if (response.ok) {
-    return response.json();
+      return response.json();
     } else {
-    throw new Error("Failed to fetch pasien data.");
+      throw new Error("Failed to fetch pasien data.");
     }
-}
-
+  }
+  
 function pushData(){
     let kodepoliklinikValue = getValue("kode_poliklinik");
     let namapoliklinikValue = getValue("nama_poliklinik");

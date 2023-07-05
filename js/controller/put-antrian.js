b5b82197d3f7a3ad73f6412d487d436f44f523cd
Dimas Ardianto <dimasardnt6@gmail.com>
2023-07-05 14:32:26 +0700

    push

--- js/controller/put-antrian.js
+++ js/controller/put-antrian.js
@@ -1,92 +1,100 @@
 import { putData } from "https://bukulapak.github.io/api/process.js";
 import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
-import { urlPUT, AmbilResponse} from "../config/url_put-antrian.js";
+import { urlPUT, AmbilResponse } from "../config/url_put-antrian.js";
 
 async function getpoliData(poliklinikId) {
-    // Fetch school data based on the ID (replace with your API endpoint)
-    const response = await fetch(`https://dimasardnt6-ulbi.herokuapp.com/poliklinik/${poliklinikId}`);
-    if (response.ok) {
+  const response = await fetch(
+    `https://dimasardnt6-ulbi.herokuapp.com/poliklinik/${poliklinikId}`
+  );
+  if (response.ok) {
     return response.json();
-    } else {
-    throw new Error("Failed to fetch poliklinik data.");
-    }
+  } else {
+    throw new Error("Gagal mengambil data poliklinik.");
+  }
 }
 
 async function getpasienData(pasienId) {
-    // Fetch school data based on the ID (replace with your API endpoint)
-    const response = await fetch(`https://dimasardnt6-ulbi.herokuapp.com/pasien/${pasienId}`);
-    if (response.ok) {
+  const response = await fetch(
+    `https://dimasardnt6-ulbi.herokuapp.com/pasien/${pasienId}`
+  );
+  if (response.ok) {
     return response.json();
-    } else {
-    throw new Error("Failed to fetch pasien data.");
-    }
+  } else {
+    throw new Error("Gagal mengambil data pasien.");
+  }
 }
 
-function pushData(){
+function pushData() {
+  let kodepoliklinikValue = getValue("kode_poliklinik");
+  let namapoliklinikValue = getValue("nama_poliklinik");
+  let namapasienValue = getValue("nama_pasien");
+  let nomorktpValue = getValue("nomor_ktp");
+  let nomorteleponValue = getValue("nomor_telepon");
+  let nomorantrianValue = getValue("nomor_antrian");
+  let statusantrianValue = getValue("status_antrian");
 
-    let kodepoliklinikValue = getValue("kode_poliklinik");
-    let namapoliklinikValue = getValue("nama_poliklinik");
-    let namapasienValue = getValue("nama_pasien");
-    let nomorktpValue = getValue("nomor_ktp");
-    let nomorteleponValue = getValue("nomor_telepon");
-    let nomorantrianValue = getValue("nomor_antrian");
-    let statusantrianValue = getValue("status_antrian");
-
-   // form validation
-   if (kodepoliklinikValue === "" && namapoliklinikValue === "" && namapasienValue === "" && nomorktpValue === "" && statusantrianValue === "") {
+  // Validasi form
+  if (
+    kodepoliklinikValue === "" ||
+    namapoliklinikValue === "" ||
+    namapasienValue === "" ||
+    nomorktpValue === "" ||
+    statusantrianValue === ""
+  ) {
     Swal.fire({
-    icon: 'error',
-    title: 'Data tidak boleh kosong!',
-    text: '',
-    showConfirmButton: false,
-    timer: 2000
+      icon: "error",
+      title: "Data tidak boleh kosong!",
+      text: "",
+      showConfirmButton: false,
+      timer: 2000,
     });
     return;
-}
+  }
 
-if (statusantrianValue === "") {
+  if (statusantrianValue === "") {
     Swal.fire({
-    icon: 'error',
-    title: 'Status Antrian tidak boleh kosong!',
-    text: '',
-    showConfirmButton: false,
-    timer: 2000
+      icon: "error",
+      title: "Status Antrian tidak boleh kosong!",
+      text: "",
+      showConfirmButton: false,
+      timer: 2000,
     });
-    cek = false;
-}   
+    return;
+  }
 
-Promise.all([getpoliData(kodepoliklinikValue,namapoliklinikValue), getpasienData(namapasienValue,nomorktpValue,nomorteleponValue)])
-.then(([poliData, pasienData]) => {
-  // Ekstrak nilai-nilai yang diperlukan dari data yang diambil
-    let kodepoliText = poliData.kode_poliklinik;
-    let namapoliValue = poliData.nama_poliklinik;
+  Promise.all([
+    getpoliData(kodepoliklinikValue),
+    getpasienData(namapasienValue),
+  ])
+    .then(([poliData, pasienData]) => {
+      let kodepoliText = poliData.kode_poliklinik;
+      let namapoliValue = poliData.nama_poliklinik;
 
-    let namapasienText = pasienData.nama_pasien;
-    let noktpValue = pasienData.nomor_ktp;
-    let noteleponValue = pasienData.nomor_telepon;
+      let namapasienText = pasienData.nama_pasien;
+      let noktpValue = pasienData.nomor_ktp;
+      let noteleponValue = pasienData.nomor_telepon;
 
-    // Bangun objek data
-    let data = {
-        poli:{
-            _id: kodepoliklinikValue,
-            kode_poliklinik: kodepoliText,
-            nama_poliklinik: namapoliValue,
+      let data = {
+        poli: {
+          _id: kodepoliklinikValue,
+          kode_poliklinik: kodepoliText,
+          nama_poliklinik: namapoliValue,
         },
-        identitas_pasien:{
-            _id: namapasienValue,
-            nama_pasien: namapasienText,
-            nomor_ktp: noktpValue,
-            nomor_telepon: noteleponValue,
+        identitas_pasien: {
+          _id: namapasienValue,
+          nama_pasien: namapasienText,
+          nomor_ktp: noktpValue,
+          nomor_telepon: noteleponValue,
         },
         nomor_antrian: nomorantrianValue,
         status_antrian: statusantrianValue,
-    };
-    console.log(data);
-    putData(urlPUT, data, AmbilResponse);
-})
-.catch((error) => {
-console.error(error);
-});
+      };
+      console.log(data);
+      putData(urlPUT, data, AmbilResponse);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
 onClick("button", pushData);
